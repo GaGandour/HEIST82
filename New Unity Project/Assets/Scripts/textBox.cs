1e1416509e806df4582b405b8cf29696c3b65915
Juan <libonatti.ocupacional@gmail.com>
2021-10-16 22:43:12 -0300

    😡🖉<==xXx_XtremeCommit_xXx==>🖉😡

--- New Unity Project/Assets/Scripts/textBox.cs
+++ New Unity Project/Assets/Scripts/textBox.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class textBox : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typeSpeed;
     void Start()
     {
-        
+        StartCoroutine(textTyper());
+    }
+    IEnumerator textTyper()
+     {
+        foreach(char letter in sentences[index].ToCharArray()){
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typeSpeed);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void nextSentence()
     {
-        
+        if(index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(textTyper());
+        }
+        else{
+            textDisplay.text = "";
+        }
     }
 }
