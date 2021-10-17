48f83925db17f4f32d8931cc5fb2dfb043d8ece2
GaGandour <64374553+GaGandour@users.noreply.github.com>
2021-10-16 22:44:18 -0300

    ramos

---
+++ New Unity Project/Assets/Scripts/Ramos.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+List<Fala> historia = new List<Fala> { "string1", "string2", };
+class Fala
+{
+    string id;
+    string fala;
+    List<string> listaDeOpcoes;
+    List<Fala> listaDeRamos;
+}
