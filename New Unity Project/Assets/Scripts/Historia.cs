719d431af4192b717aed15cf3ec41f6b706d32d4
GaGandour <64374553+GaGandour@users.noreply.github.com>
2021-10-18 09:40:42 -0300

    erro em hist

--- New Unity Project/Assets/Scripts/Historia.cs
+++ New Unity Project/Assets/Scripts/Historia.cs
@@ -8,12 +8,47 @@ public class Historia : MonoBehaviour
 {
     private Ramos arvore;
     private Fala falaAtual;
+    private int escolha;
+    private int probabilidade;
+    private Random rnd;
     private void Start() {
+        escolha = -1;
         arvore = gameObject.GetComponent<Ramos>() as Ramos;
         falaAtual = arvore.historia;
+        rnd = new Random();
     }
     public void IrParaOProximo () {
-        animator.SetBool("clickedPlay",true);
-        SceneManager.LoadScene("Cena Inicial");
+        switch (falaAtual.status)
+        {
+            case Status.Falha:
+                break;
+            case Status.Sucesso:
+                break;
+            case Status.Nulo:
+                switch (falaAtual.proximo)
+                {
+                    case Proximo.Escolha:
+                        // Animacao();
+                        break;
+                    case Proximo.Probabilistico:
+                        //Animacao();
+                        probabilidade = rnd.Next(1,100);
+                        for (int i = 0; i < 5; i++)
+                        {
+                            if (probabilidade <= falaAtual.listaDeProbabilidades[i])
+                            {
+                                escolha = i;
+                                break;
+                            }
+                            else
+                            {
+                                probabilidade -= falaAtual.listaDeProbabilidades[i];
+                            }
+                        }
+                        break;
+                }
+                falaAtual = falaAtual.listaDeRamos[escolha];
+                break;
+        }
     }
 }
